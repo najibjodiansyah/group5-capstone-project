5d24e3099aefb9409889b6abb8cea079a93c1a94
eldy.hidayatquery{ <eldy.hi@gmail.com>
2022-03-02 10:26:42 +0700

    fix bug get by id update items

--- entities/item.go
+++ entities/item.go
@@ -3,7 +3,7 @@ package entities
 type Item struct {
 	ID              int    `json:"id" form:"id"`
 	AssetId         int    `json:"assetId" form:"assetId"`
-	EmployeeId      int    `json:"employeeId" form:"employeeId"`
+	EmployeeId      *int   `json:"employeeId" form:"employeeId"`
 	Name            string `json:"name" form:"name"`
 	AvailableStatus string `json:"availableStatus" form:"availableStatus"`
 }
